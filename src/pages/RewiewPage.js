9894471a9f9ef85de7e9fe515815fa4c795599b2
Jora947 <JoraKochkin@yandex.ru>
2023-01-26 01:40:44 +0500

    end

---
+++ src/pages/RewiewPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../rewiew.css"
+const Rewiew = () =>{
+    return(
+        <div class="list__games">
+        <div class="center">
+        <div class="heading">Рецензии</div>
+        <div class="sort__review">
+            <div>Популярные</div>
+            <div>Новые</div>
+        </div>
+        <div class="review__list">
+            <div></div>
+            <div></div>
+        </div>
+        </div>
+    </div>
+    )
+}
+
+export default Rewiew;
